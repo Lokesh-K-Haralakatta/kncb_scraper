f2aece32448fe3cb5f3cfbf15e06041409259989
Lokesh K Haralakatta <18327888+Lokesh-K-Haralakatta@users.noreply.github.com>
2026-04-21 22:56:47 +0200

    Create TokenManager.java

---
+++ java/src/main/java/com/example/kncb/TokenManager.java
@@ -0,0 +1,13 @@
+package com.example.kncb;
+
+public class TokenManager {
+    private String token;
+
+    public TokenManager(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
