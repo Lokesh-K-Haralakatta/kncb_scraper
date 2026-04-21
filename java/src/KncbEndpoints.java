19dafd767f8f58da86f892576d1aec05dd1fd012
Lokesh K Haralakatta <18327888+Lokesh-K-Haralakatta@users.noreply.github.com>
2026-04-21 22:50:18 +0200

    Add KncbEndpoints class containing endpoint constants

---
+++ java/src/KncbEndpoints.java
@@ -0,0 +1,5 @@
+public class KncbEndpoints {
+    public static final String BASE_URL = "https://api.example.com/";
+    public static final String FETCH_DATA = BASE_URL + "data/fetch";
+    public static final String SUBMIT_DATA = BASE_URL + "data/submit";
+}
