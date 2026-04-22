ad3f252f7d9044fb1ebaff3b20f6cbb0a2eea8f0
Lokesh K Haralakatta <18327888+Lokesh-K-Haralakatta@users.noreply.github.com>
2026-04-22 21:29:59 +0200

    Create KncbEndpoints.java with API endpoint constants

---
+++ java/src/KncbEndpoints.java
@@ -0,0 +1,8 @@
+public class KncbEndpoints {
+    public static final String PLAYER_STATS = "report/rpt_cba/+";
+    public static final String MATCH_SUMMARIES = "report/rpt_mas/+";
+    public static final String BALL_BY_BALL = "report/rpt_bbb/+";
+    public static final String GRADES = "grades/+";
+    public static final String SEASONS = "seasons/+";
+    public static final String MATCH_BREAK_TYPES = "match_break_types/+";
+}
